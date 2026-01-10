f9a3971f5e423d8adece397c7719af16ccb0fd47
zaid-alrafayia <zaidomar678@gmail.com>
2026-01-10 13:11:06 +0300

    a

--- process.c
+++ process.c
@@ -13,12 +13,28 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+static void	close_fd(t_pipex *px, int a)
+{
+	close(px->fd[0]);
+	close(px->fd[1]);
+	if (a)
+	{
+		close(px->infd);
+		close(px->outfd);
+	}
+}
+
 void	child_process(t_pipex *px, int i)
 {
 	char	*cmd_pt;
 	char	**split_cmd;
 
 	split_cmd = cmd_split(px->argv[i]);
+	if (!split_cmd)
+	{
+		close_fd(px, 0);
+		error_exit(px, "cmd split error", 2);
+	}
 	cmd_pt = cmd_path(px, split_cmd[0]);
 	if (i == 2)
 	{
@@ -32,10 +48,7 @@ void	child_process(t_pipex *px, int i)
 		dup2(px->fd[0], STDIN_FILENO);
 		dup2(px->outfd, STDOUT_FILENO);
 	}
-	close(px->fd[0]);
-	close(px->fd[1]);
-	close(px->infd);
-	close(px->outfd);
+	close_fd(px, 1);
 	execve(cmd_pt, split_cmd, px->envp);
 	exit(1);
 }
